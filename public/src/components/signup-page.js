200e580bd40a906eca8d74b646d39d5daca11218
preservance717 <GL717@outlook.com>
2016-08-11 01:03:19 +0800

    modify drop-down box

--- public/src/components/signup-page.js
+++ public/src/components/signup-page.js
@@ -16,14 +16,8 @@ export default class SignUpPage extends Component {
           <option value="opel">60-65</option>
           <option value="audi">65-70</option>
         </select>
-          兴趣<Select options={[{value: 'one', label: 'One'},
-          {value: 'two', label: 'Two'},
-          {value: 'three', label: 'Three'},
-          {value: 'a', label: 'Apple'},
-          {value: 'b', label: 'Banana'},
-          {value: 'o', label: 'Orange'},
-          {value: 'm', label: 'Mango'}]}/>
-          城市<Select options={[{value: 'one', label: '西安'}]}/>
+          兴趣<HobbyMultiSelectField />
+          城市<CitySelectField/>
           密码 <input type="password" />
           确认密码 <input type="password" />
           <Link to="/">返回</Link>
@@ -34,3 +28,57 @@ export default class SignUpPage extends Component {
   }
 }
 
+const HobbyMultiSelectField = React.createClass({
+  getInitialState(){
+    return {
+      disabled: false,
+      options: [
+        { label: '下棋', value: 'chess' },
+      	{ label: '打太极', value: 'taiji' },
+      	{ label: '打牌', value: 'cards' },
+      	{ label: '跳广场舞', value: 'dance' },
+      ],
+      value: []
+    };
+  },
+  handleSelectChange (value) {
+		this.setState({ value });
+	},
+  render:function () {
+    return (<div className='section'>
+    <Select multi simpleValue disabled={this.state.disabled} value={this.state.value}
+     placeholder="Select your favourite(s)" options={this.state.options}
+     onChange={this.handleSelectChange} />
+  </div>)
+  }
+})
+
+const CitySelectField = React.createClass({
+  getInitialState:function(){
+  return {
+    value:''
+  }
+},
+  setValue (value) {
+  this.setState({ value });
+},
+  render:function () {
+  var options = [
+    { label: '西安', value: 'basic'},
+    { label: '北京', value: 'premium'},
+    { label: '沈阳', value: 'pro' },
+  ];
+  return (
+    <div className="section">
+      <h3 className="section-heading">{this.props.label}</h3>
+      <Select
+        placeholder="Select your province"
+        options={options}
+        optionRenderer={this.renderOption}
+        onChange={this.setValue}
+        value={this.state.value}
+        />
+    </div>
+  );
+ }
+})
