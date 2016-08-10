8f5a98fd96b4a31cc43608c39f1fdaf26aa6cb0c
XLuoChen <1425145741@qq.com>
2016-08-11 01:38:02 +0800

    add modal when click recommend friends button

--- public/src/components/personpage.js
+++ public/src/components/personpage.js
@@ -2,11 +2,24 @@ import React, {Component} from "react";
 import {Link} from 'react-router';
 
 export default class PersonPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isWantToFindFriends: false
+    }
+  }
+
+  findFriends() {
+    this.setState({isWantToFindFriends: !this.state.isWantToFindFriends});
+    console.log(this.state.isWantToFindFriends);
+  }
+
   render() {
     return (
       <div>
         <Header />
-        <Mainer />
+        <Mainer isWantToFindFriends={this.state.isWantToFindFriends}
+                findFriends={this.findFriends.bind(this)}/>
         <Footer />
       </div>
     )
@@ -35,13 +48,17 @@ class Header extends Component {
 class Mainer extends Component {
   render() {
     return <div>
-      <Left />
-      <Right />
+      <Left findFriends={this.props.findFriends}/>
+      <Right isWantToFindFriends={this.props.isWantToFindFriends} findFriends={this.props.findFriends}/>
     </div>
   }
 }
 
 class Left extends Component {
+  toFindFriends() {
+    this.props.findFriends();
+  }
+
   render() {
     return <div className="col-lg-4">
       <div id="leftOfPersonPage">
@@ -50,7 +67,7 @@ class Left extends Component {
         </div>
         <div>
           <ul className="list-group">
-            <li className="list-group-item"><a id="popover">推荐好友</a></li>
+            <li className="list-group-item"><a onClick={this.toFindFriends.bind(this)}> 推荐好友</a></li>
             <li className="list-group-item"><a>我的好友</a></li>
             <li className="list-group-item"><a>我的动态</a></li>
             <li className="list-group-item"><a>个人信息</a></li>
@@ -69,6 +86,9 @@ class Left extends Component {
 class Right extends Component {
   render() {
     return <div className="col-lg-8">
+      <div className={this.props.isWantToFindFriends ? '' : 'hidden'}>
+        <OptionsToFind findFriends={this.props.findFriends}/>
+      </div>
     </div>
   }
 }
@@ -77,6 +97,56 @@ class Footer extends Component {
   render() {
     return <div className="col-lg-12" id="footerOfPersonPage">
       版权所有@sunset
+    </div>
+  }
+}
+
+class OptionsToFind extends Component {
+  closeModal() {
+    this.props.findFriends();
+  }
+
+  render() {
+    return <div className="modal-dialog" id="optionsModal">
+      <div className="modal-content">
+        <div className="modal-header">
+          <button type="button" className="close" data-dismiss="modal"
+                  aria-hidden="true" onClick={this.closeModal.bind(this)}>×
+          </button>
+          <h4 className="modal-title" id="myModalLabel">
+            请选择：
+          </h4>
+        </div>
+        <div className="col-sm-offset-1">
+          <form className="form-inline" role="form">
+            <div>
+              城市：
+              <input type="checkbox"/>上海
+              <input type="checkbox"/>北京
+            </div>
+            <div>
+              年龄段：
+              <input type="checkbox"/>55~60
+              <input type="checkbox"/>60~65
+              <input type="checkbox"/>65~70
+            </div>
+            <div>
+              兴趣爱好：
+              <input type="checkbox"/>运动
+              <input type="checkbox"/>下棋
+              <input type="checkbox"/>看书
+            </div>
+          </form>
+        </div>
+        <div className="modal-footer">
+          <button type="button" className="btn btn-default"
+                  data-dismiss="modal">关闭
+          </button>
+          <button type="button" className="btn btn-primary">
+            确定
+          </button>
+        </div>
+      </div>
     </div>
   }
 }
