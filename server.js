9f9e1eecab0aa352e9b65661902b8c84619bf1dd
XLuoChen <1425145741@qq.com>
2016-08-11 14:36:01 +0800

    change footer and header color of personpage

--- server.js
+++ server.js
@@ -11,13 +11,7 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler));
 
-
-
 app.use(express.static('public'));
-
-app.get('/', (res, req)=> {
-  req.send('abc');
-});
 
 app.listen(3000, function() {
   console.log("server started at http://localhost:3000");
