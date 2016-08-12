fcc643d7ad3ddab84d53ec3865b8e117473d2f94
preservance717 <GL717@outlook.com>
2016-08-12 17:07:11 +0800

    verify front message about phone password confirmPassword  Please enter the commit message for your changes. Lines starting

--- public/src/components/signup-page.js
+++ public/src/components/signup-page.js
@@ -6,14 +6,14 @@ export default class SignUpPage extends Component {
   constructor(props){
     super(props);
     this.state = {
-      '_id':'',
-      name:'',
-      sex:'',
-      pw:'',
-      cfpw:'',
-      hobbies:[],
-      age:'',
-      city:''
+          '_id':'',
+          name:'',
+          sex:'',
+          password:'',
+          cfpw:'',
+          hobbies:[],
+          age:'',
+          city:''
     }
   }
 
@@ -21,16 +21,52 @@ export default class SignUpPage extends Component {
     this.setState({'_id':event.target.value})
   }
 
+  verifyId(event){
+    if(event.key=='Enter'){
+      const regu = /^1\d{10}$/;
+      const re = new RegExp(regu);
+      console.log(this.state._id+"wwwww")
+      if(re.test(this.state._id)){
+          $("input[id=nickname]").focus();
+      }else{
+        alert('请填写有效的11位手机号码');
+      }
+    }
+  }
+
   handleName(event){
     this.setState({name:event.target.value})
   }
 
   handlePassword(event){
-    this.setState({pw:event.target.value})
+    this.setState({password:event.target.value})
+  }
+
+  verifyPw(event){
+    if(event.key=='Enter'){
+      const regu = /^[0-9A-Za-z]{6,20}$/;
+      const re = new RegExp(regu);
+
+      if(re.test(this.state.password)){
+          $("input[id=cfpw]").focus();
+      }else{
+        alert('请输入6-20个数字、字母(区分大小写)');
+      }
+    }
   }
 
   confirmPassword(event){
     this.setState({cfpw:event.target.value})
+  }
+  confirmPw(event){
+    if(event.key=='Enter'){
+      if(this.state.cfpw === this.state.password){
+        $("input[id=age]").focus();
+      }else{
+        alert("确认密码和密码不符合");
+        $("input[id=cfpw]").val('');
+      }
+    }
   }
 
   handleHobby(hobbies){
@@ -47,8 +83,17 @@ export default class SignUpPage extends Component {
 
   sendData(){
     this.state.sex = $("input[name=sex]:checked").val();
+    const messages = {
+      '_id':this.state._id,
+      name:this.state.name,
+      sex:this.state.sex,
+      password:this.state.password,
+      hobbies:this.state.hobbies,
+      age:this.state.age,
+      city:this.state.city
+    }
 
-    $.post('/', this.state, (data)=>{
+    $.post('/', messages, (data)=>{
     //  console.log(data);
     })
   }
@@ -61,12 +106,12 @@ export default class SignUpPage extends Component {
 
           <div className="input-group">
             <span className="input-group-addon">手机号</span>
-            <input type="text" className="form-control" onChange={this.handleId.bind(this, event)}/>
+            <input type="text" id='phoneNumber' className="form-control" onChange={this.handleId.bind(this)} onKeyPress={this.verifyId.bind(this)}/>
           </div>
           <br/>
           <div className="input-group">
             <span className="input-group-addon">昵称</span>
-            <input type="text" className="form-control" onChange={this.handleName.bind(this, event)}/>
+            <input type="text" id='nickname' className="form-control" onChange={this.handleName.bind(this, event)}/>
           </div>
           <br/>
           <div className="input-group">
@@ -77,19 +122,19 @@ export default class SignUpPage extends Component {
           <br/>
           <div className="input-group">
             <span className="input-group-addon">密码</span>
-            <input type="password" className="form-control" onChange={this.handlePassword.bind(this, event)}/>
+            <input type="password" id='pw' className="form-control" onChange={this.handlePassword.bind(this, event)} onKeyPress={this.verifyPw.bind(this)}/>
           </div>
           <br/>
           <div className="input-group">
             <span className="input-group-addon">确认密码</span>
-            <input type="password" className="form-control" onChange={this.confirmPassword.bind(this, event)}/>
+            <input type="password" id='cfpw' className="form-control" onChange={this.confirmPassword.bind(this, event)} onKeyPress={this.confirmPw.bind(this)}/>
           </div>
           <br/>
-          <AgeSelectField onChange={this.handleAge.bind(this)}/>
+          <AgeSelectField id='age' onChange={this.handleAge.bind(this)}/>
           <br/>
-          <HobbyMultiSelectField onChange={this.handleHobby.bind(this)}/>
+          <HobbyMultiSelectField id='hobby' onChange={this.handleHobby.bind(this)}/>
           <br/>
-          <CitySelectField onChange={this.handleCity.bind(this)}/>
+          <CitySelectField id='city' onChange={this.handleCity.bind(this)}/>
           <br/>
 
           <div className="col-lg-5 col-lg-offset-3">
@@ -155,7 +200,8 @@ export class CitySelectField extends Component {
 
   setCity(city) {
     this.setState({city});
-    this.props.onChange(city);
+    console.log(city)
+    this.props.onChange(city.label);
   }
 
   render() {
@@ -188,7 +234,7 @@ export class AgeSelectField extends Component {
 
   setAge(age) {
     this.setState({age});
-    this.props.onChange(age)
+    this.props.onChange(age.label)
   }
 
   render() {
