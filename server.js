da1be0fb994cb519c5e9929d84c0c2c440b59da8
fat-gril <15809244435@163.com>
2016-08-12 17:44:45 +0800

    add CI test

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
-import express from 'express';
-import webpack from 'webpack';
-import webpackConfig from './webpack.config';
-import signup from './routes/sign-up.js'
-import bodyParser from 'body-parser';
+function makeServer() {
+  const express = require('express');
+  const webpack = require('webpack');
+  const webpackConfig = require('./webpack.config');
+  const signup = require('./routes/sign-up.js');
+  const bodyParser = require('body-parser');
 
-const app = express();
-const compiler = webpack(webpackConfig);
+  const app = express();
+  const compiler = webpack(webpackConfig);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: webpackConfig.output.publicPath
-}));
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath
+  }));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(require('webpack-hot-middleware')(compiler));
+  app.use(require('webpack-hot-middleware')(compiler));
 
-app.use(express.static('public'));
+  app.use(express.static('public'));
 
-app.use('/', signup);
-app.use(require('./routes/find-friends'));
+  app.use('/', signup);
+  app.use(require('./routes/find-friends'));
 
-app.listen(3000, function() {
-  console.log("server started at http://localhost:3000");
-});
+  const server = app.listen(3000, function () {
+    var port = server.address().port;
+    console.log('Example app listening at port %s', port);  });
+
+  return server;
+}
+module.exports = makeServer;
