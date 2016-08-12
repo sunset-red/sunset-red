88bb9ea78be59b2810816376231bbb0bf8931920
XLuoChen <1425145741@qq.com>
2016-08-13 01:10:25 +0800

    revert integration test

--- test/test.js
+++ test/test.js
@@ -11,11 +11,18 @@ describe('loading express', function () {
     server.close(done);
   });
 
-  it('responds to /friends', function testSlash(done) {
+  it('responds to /', function testSlash(done) {
     this.timeout(35000);
     request(server)
-      .post('/friends')
-      .expect(200, done);
+      .get('/')
+      .expect(200, done)
+  });
+
+  it('404 everything else', function testPath(done) {
+    this.timeout(35000);
+    request(server)
+      .get('/foo/bar')
+      .expect(404, done);
   });
 
 });
